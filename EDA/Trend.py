eedf6004219dccbf4f7d5062d017dd307ea878ad
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2022-08-07 01:18:16 +0530

    installed jupytext, added an EDA file

---
+++ EDA/Trend.py
@@ -0,0 +1,27 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+data = pd.read_csv('../data/ge.us.txt')
+
+# %%
+data
+
+# %%
+plt.plot(data.Open)
